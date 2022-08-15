7e3f84117caa08a24c8d2c21d96e555d84d46aa4
Peter1907 <peter.nady.1408@gmail.com>
2022-08-15 22:50:09 +0200

    Add the code from to-do-list

---
+++ index.js
@@ -0,0 +1,23 @@
+import './style.css';
+import {
+  display, add, modify, removeItem,
+} from './functions.js';
+import { update } from './updates.js';
+
+const TodoItemsV2 = JSON.parse(localStorage.getItem('ToDoItems')) || [];
+if (TodoItemsV2.length > 0) {
+  for (let i = 0; i < TodoItemsV2.length; i += 1) {
+    display(i);
+    modify(i);
+  }
+  removeItem();
+  update();
+}
+
+const input = document.getElementById('add');
+const submit = document.getElementById('submit');
+submit.addEventListener('click', () => {
+  const description = input.value;
+  add(description);
+  input.value = '';
+});
